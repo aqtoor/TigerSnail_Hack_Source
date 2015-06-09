146ec2be446c6ad4e763bf1a12dd24e079839c83
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-06-09 14:50:27 +0200

    Create utils.h

---
+++ utils.h
@@ -0,0 +1,24 @@
+#ifndef _UTILS_H
+#define _UTILS_H
+
+#include "includes.h"
+
+/*
+	TigerSnail Hack: Source
+
+	Developed by sk0r / Czybik
+	Credits: sk0r, OllyDbg, Source SDK
+
+	Version: 0.1
+	Visit: http://sk0r.sytes.net
+	Mail Czybik_Stylez<0x40>gmx<0x2E>de
+
+	utils.h: Utility interface
+*/
+
+//======================================================================
+bool FileExists(const std::string& szFileName);
+bool DeleteFileObject(const std::string& szObject);
+//======================================================================
+
+#endif
