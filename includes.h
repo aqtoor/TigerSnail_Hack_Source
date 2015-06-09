6d8b6ec44735280d668395e2715e90cb3b460d90
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-06-09 14:52:13 +0200

    Create includes.h

---
+++ includes.h
@@ -0,0 +1,25 @@
+#ifndef _INCLUDES_H
+#define _INCLUDES_H
+
+#include <cstdlib>
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include <Windows.h>
+
+/*
+	TigerSnail Hack: Source
+
+	Developed by sk0r / Czybik
+	Credits: sk0r, OllyDbg, Source SDK
+
+	Version: 0.1
+	Visit: http://sk0r.sytes.net
+	Mail Czybik_Stylez<0x40>gmx<0x2E>de
+
+	File: includes.h: General include files 
+*/
+
+#endif
