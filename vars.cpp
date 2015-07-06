87a0baeb113e247f297157b4cda2255d2d49b45f
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-07-06 20:02:53 +0200

    Update vars.cpp

--- vars.cpp
+++ vars.cpp
@@ -6,7 +6,7 @@
 	Developed by sk0r / Czybik
 	Credits: sk0r, OllyDbg, Source SDK
 
-	Version: 0.3
+	Version: 0.4
 	Visit: http://sk0r.sytes.net
 	Mail Czybik_Stylez<0x40>gmx<0x2E>de
 
@@ -44,6 +44,8 @@ screensize_s g_ScreenSize; //Screen resolution
 bool g_bMapInit = false; //Whether map has been initialized
 bool g_bOnInitialize = false; //Whether OnInitialize() has already been called
 bool g_bMenuToggle = false; //Menu toggle indicator
+bool g_bSnakeToggle = false; //Snake toggle indicator
+bool g_bInfoboxToggle = false; //Infobox toggle indicator
 bool g_bIsInGame = false; //Whether in-game context is given or not
 
 CLog* g_pLog = NULL; //Log manager instance
@@ -55,9 +57,19 @@ CPlayerMgr g_oPlayerMgr; //Player manager class instance
 CzyConfigMgr::CCVar::cvar_s* g_pcvNameESP = NULL; //CVar for name esp
 CzyConfigMgr::CCVar::cvar_s* g_pcvSteamIDESP = NULL; //CVar for SteamID esp
 CzyConfigMgr::CCVar::cvar_s* g_pcvDistanceESP = NULL; //CVar for distance esp
-CzyConfigMgr::CCVar::cvar_s* g_pcvIgnoreTeammatesESP = NULL; //CVar to indicate whether ignore teammates for ESP or not
+CzyConfigMgr::CCVar::cvar_s* g_pcvHealthESP = NULL; //CVar for health esp
+CzyConfigMgr::CCVar::cvar_s* g_pcvDecoyESP = NULL; //CVar for decoy esp
+CzyConfigMgr::CCVar::cvar_s* g_pcvBombESP = NULL; //CVar for bomb esp
+CzyConfigMgr::CCVar::cvar_s* g_pcvIgnoreTeammatesESP = NULL; //CVar to indicate whether ignore teammates for esp or not
 CzyConfigMgr::CCVar::cvar_s* g_pcvColorModeESP = NULL; //CVar to indicate whether use team related or player context team related colors
 CzyConfigMgr::CCVar::cvar_s* g_pcvMenuKey = NULL; //CVar for menu key
+CzyConfigMgr::CCVar::cvar_s* g_pcvSnakeKey = NULL; //CVar for snake key
+CzyConfigMgr::CCVar::cvar_s* g_pcvInfoboxKey = NULL; //CVar for infobox key
+CzyConfigMgr::CCVar::cvar_s* g_pcvSnakeVelocity = NULL; //CVar for snake velocity
 
 std::string g_szCurrentMapName(""); //Current map name
+
+CPlayTime g_oPlayTime; //Playtime manager
+accuracy_s g_sAccuracy; //Accuracy data
+bomb_data_s g_sBombData = {false, false, false, NULL, -1, 0}; //Bomb data
 //======================================================================
