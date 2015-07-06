071506edb836ab6258985af88c496e8dd0ceb62f
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-07-06 19:59:10 +0200

    Update drawing.cpp

--- drawing.cpp
+++ drawing.cpp
@@ -6,9 +6,9 @@
 	Developed by sk0r / Czybik
 	Credits: sk0r, OllyDbg, Source SDK
 
-	Version: 0.3
+	Version: 0.4
 	Visit: http://sk0r.sytes.net
-	Mail Czybik_Stylez@gmx.de
+	Mail Czybik_Stylez<0x40>gmx<0x2E>de
 
 	drawing.cpp: Game Engine drawing implementation
 */
