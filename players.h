c602cc5d6f80ecdf66fc3bd898fd304047d2736b
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-07-06 19:54:45 +0200

    Update players.h

--- players.h
+++ players.h
@@ -10,7 +10,7 @@
 	Developed by sk0r / Czybik
 	Credits: sk0r, OllyDbg, Source SDK
 
-	Version: 0.3
+	Version: 0.4
 	Visit: http://sk0r.sytes.net
 	Mail Czybik_Stylez<0x40>gmx<0x2E>de
 
@@ -31,6 +31,9 @@
 #define LINE_DISTANCE 25
 #define LINE_OFFSET(v) (v * LINE_DISTANCE)
 #define PLAYER_TEAM_VISIBLE PLAYER_TEAM_SPEC
+
+#define COLOR_OVERWRITE_NO this->m_sNoColorOverwrite
+#define COLOR_OVERWRITE_GREY this->m_sGreyColor
 //======================================================================
 
 //======================================================================
@@ -67,12 +70,14 @@ class CPlayerMgr {
 private:
 	player_data_s m_sPlayerData[MAX_CLIENTS];
 	color24 m_sTeamColors[3];
+	color32_s m_sNoColorOverwrite;
+	color32_s m_sGreyColor;
 
 	bool CalcScreen(const Vector& vWorldPos, Vector& vScreenPos);
 	bool UpdateCurWeapon(const MPLAYERID uiEntIndex);
 	bool IsVisible(const MPLAYERID uiEntIndex);
 public:
-	CPlayerMgr() { this->Reset(); }
+	CPlayerMgr() { this->Reset(); this->m_sNoColorOverwrite.a = 0xFF; this->m_sGreyColor.r = this->m_sGreyColor.g = this->m_sGreyColor.b = 200; this->m_sGreyColor.a = 0; }
 	~CPlayerMgr() {}
 
 	void SetTeamColor(const byte ucSlot, const color24& sColor) { if (ucSlot >= 3) return; this->m_sTeamColors[ucSlot] = sColor; }
@@ -94,11 +99,14 @@ public:
 
 	float GetDistance(const MPLAYERID uiPlayerSrc, const MPLAYERID uiPlayerDst);
 
-	bool DrawInfo(const MPLAYERID uiEntIndex, const std::string& szText, const byte ucLineId/*, const bool bCentered*/);
+	bool DrawInfo(const MPLAYERID uiEntIndex, const std::string& szText, const byte ucLineId, const color32_s& clrColor/*, const bool bCentered*/);
 	bool DrawName(const MPLAYERID uiEntIndex, const byte ucLineId/*, const bool bCentered*/);
 	bool DrawDistance(const MPLAYERID uiEntIndex, const byte ucLineId/*, const bool bCentered*/);
 	bool DrawWeapon(const MPLAYERID uiEntIndex, const byte ucLineId/*, const bool bCentered*/);
 	bool DrawSteamID(const MPLAYERID uiEntIndex, const byte ucLineId/*, const bool bCentered*/);
+	bool DrawHealth(const MPLAYERID uiEntIndex, const byte ucLineId/*, const bool bCentered*/);
+
+	int GetLocalPlayerTeam(void) { player_data_s* pLocalPlayerData = this->Local(); if (pLocalPlayerData) return pLocalPlayerData->iTeam; return -1; }
 
 	color24 MakeColor24(int r, int g, int b)
 	{ 
