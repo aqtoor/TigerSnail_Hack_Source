8782845d29bb65025fe17f2065c7a83abc2e2c9f
sk0r Czybik <sk0r-Czybik@users.noreply.github.com>
2015-07-06 19:56:38 +0200

    Update inputmgr.h

--- inputmgr.h
+++ inputmgr.h
@@ -8,9 +8,9 @@
 	TigerSnail Hack: Source
 
 	Developed by sk0r / Czybik
-	Credits: sk0r, OllyDbg, Source SDK
+	Credits: sk0r, OllyDbg, SourceSDK
 
-	Version: 0.3
+	Version: 0.4
 	Visit: http://sk0r.sytes.net
 	Mail Czybik_Stylez<0x40>gmx<0x2E>de
 
